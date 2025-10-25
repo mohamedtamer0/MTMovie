99f456f005184b9a94202dc2011835d6224e9a98
mohamedtamer0 <mt4452242@gmail.com>
2025-10-25 22:17:50 +0300

    Update

---
+++ MTMovie/HorizontalListView.swift
@@ -0,0 +1,18 @@
+//
+//  HorizontalListView.swift
+//  MTMovie
+//
+//  Created by Mohamed Tamer on 25/10/2025.
+//
+
+import SwiftUI
+
+struct HorizontalListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HorizontalListView()
+}
